a49fd0c737fb2141c71cb88ecacee4aab9f86ab8
wky <wky_0615@163.com>
2017-12-21 17:12:55 +0800

    update

--- game.js
+++ game.js
@@ -1,23 +1,82 @@
-var canvas = document.querySelector('#canvas')
-var ctx = canvas.getContext('2d')
-
-var img = new Image()
-img.src = "./images/paddle.png"
-var x = 150
-var y = 300
-img.onload = function(){
-  ctx.drawImage(img, x, y)
+var imageFromPath = function(path){
+  var img = new Image()
+  img.src = path
+  return img
 }
 
-window.addEventListener('keydown', function(event){
-  var key = event.key
-  if(key === 'ArrowLeft'){
-    x -= 10
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(img, x, y)
-  }else if(key === 'ArrowRight'){
-    x += 10
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(img, x, y)
+var Paddle = function() {
+  var image = imageFromPath('./images/paddle.png')
+  var obj = {
+    image: image,
+    x: 150,
+    y: 350,
+    speed: 5,
   }
-})
+  obj.moveLeft = function(){
+    obj.x -= obj.speed
+  }
+  obj.moveRight = function(){
+    obj.x += obj.speed    
+  }
+  return obj
+}
+
+var Game = function(){
+  var g = {
+    actions: {
+      
+    },
+    keydown: {
+      
+    }
+  }
+  var canvas = document.querySelector('#canvas')
+  var ctx = canvas.getContext('2d')
+  g.canvas = canvas
+  g.ctx = ctx
+  setInterval(function(){
+    //update
+    g.update()
+    //clear
+    ctx.clearRect(0, 0, canvas.width, canvas.height)  
+    //draw
+    g.draw()
+  }, 1000/30)
+
+  return g
+}
+var APP = function(){
+  var game = Game()
+  var paddle = Paddle()
+
+  var leftDown = false
+  var rightDown = false
+  window.addEventListener('keydown', function(event){
+    var key = event.key
+    if(key === 'ArrowLeft'){
+      leftDown = true
+    }else if(key === 'ArrowRight'){
+      rightDown = true
+    }
+  })
+  window.addEventListener('keyup', function(event){
+    var key = event.key
+    if(key === 'ArrowLeft'){
+      leftDown = false
+    }else if(key === 'ArrowRight')
+      rightDown = false
+  })
+  game.update = function(){
+    if(leftDown){
+      paddle.moveLeft()
+    }else if(rightDown){
+      paddle.moveRight()
+    }
+  }
+  game.draw = function(){
+    game.ctx.drawImage(paddle.image, paddle.x, paddle.y)    
+  }
+
+}
+
+APP()
